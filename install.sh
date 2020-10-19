fa3918e6e582bda25c732a19a8a250080f309047
StellarCollision <stellarcollision@logbuch.it>
2020-10-19 22:50:41 +0200

    restructuring

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #init checks
-source ./modules/checks/init_check.sh
+source ./modules/install/checks/init_check.sh
 
 #install archlinux 
-source ./modules/initial_setup/before_check.sh
-source ./modules/initial_setup/work.sh
-source ./modules/initial_setup/after_check.sh
+source ./modules/install/init/before_check.sh
+source ./modules/install/init/work.sh
+source ./modules/install/init/after_check.sh
 
 arch-chroot /mnt/ sh /opt/ArchEvo/update.sh
