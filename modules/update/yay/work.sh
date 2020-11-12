749b39b9c8fbd4f1ba43f7b124d3159003fa7458
StellarCollision <stellarcollision@logbuch.it>
2020-11-12 18:57:53 +0100

    username variable

--- modules/update/yay/work.sh
+++ modules/update/yay/work.sh
@@ -5,9 +5,9 @@ pacman -S --noconfirm git base-devel go
 git clone https://aur.archlinux.org/yay.git /tmp/ArchEvo_yay
 
 cd /tmp/ArchEvo_yay/
-chown user. -R /tmp/ArchEvo_yay
+chown $INPUT_USER_NAME. -R /tmp/ArchEvo_yay
 
-su user -c "makepkg" # todo change username
+su $INPUT_USER_NAME -c "makepkg"
 
 pacman -U --noconfirm yay-*-x86_64.pkg.tar.zst
 
