199c9033f2fd82c67ef704ed7fcd770a232a2101
StellarCollision <stellarcollision@logbuch.it>
2020-11-17 22:54:20 +0100

    #1 gnome conf: keyboard(static), disable update by softwarecenter, numblock by boot on

--- modules/update/gnome/work.sh
+++ modules/update/gnome/work.sh
@@ -4,4 +4,10 @@ pacman -Syy
 pacman -S gnome gdm nautilus-share --noconfirm
 
 #-->Service
-systemctl enable gdm 
+systemctl enable gdm
+
+#-->Config
+pkexec
+su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.input-sources sources \"[('xkb', 'de')]\""
+su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.software download-updates false"
+su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.peripherals.keyboard numlock-state on"
