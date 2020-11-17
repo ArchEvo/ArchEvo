017a373530190127340f7bc86f0c829690aef345
StellarCollision <stellarcollision@logbuch.it>
2020-11-17 22:32:38 +0100

    #5 Time settings and NTP improved; last revert

--- modules/update/timedatectl/work.sh
+++ modules/update/timedatectl/work.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-#config root
 timedatectl set-timezone $INPUT_TIMEZONE_CONTINENT/$INPUT_TIMEZONE_CITY
 timedatectl set-ntp true
-
-#config user
-su $INPUT_USER_NAME -c "timedatectl set-timezone $INPUT_TIMEZONE_CONTINENT/$INPUT_TIMEZONE_CITY"
-su $INPUT_USER_NAME -c "timedatectl set-ntp true"
 
 timedatectl set-local-rtc 0 --adjust-system-clock
