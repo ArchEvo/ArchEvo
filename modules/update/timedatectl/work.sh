05d815c1eaeac482e7e12d8a9a7bfe26a66095b5
StellarCollision <stellarcollision@logbuch.it>
2020-11-17 15:52:44 +0100

    #2 Time settings and NTP improved

--- modules/update/timedatectl/work.sh
+++ modules/update/timedatectl/work.sh
@@ -2,4 +2,4 @@
 
 timedatectl set-timezone $INPUT_TIMEZONE_CONTINENT/$INPUT_TIMEZONE_CITY
 timedatectl set-ntp true
-timedatectl set-local-rtc 1 --adjust-system-clock
+timedatectl set-local-rtc 0 --adjust-system-clock
