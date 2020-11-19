391bc90854989fe91157d8c7ac853ac02e2c782d
StellarCollision <stellarcollision@logbuch.it>
2020-11-19 21:54:28 +0100

    fix typo

--- modules/update/reflector/work.sh
+++ modules/update/reflector/work.sh
@@ -2,4 +2,4 @@
 
 pacman -S --noconfirm reflector
 
-reflector --verbose -l 10 -a 12 --score 50 --sort rate -p https --country '$INPUT_REFLECTOR_COUNTRY'  --save /etc/pacman.d/mirrorlist
+reflector --verbose -l 10 -a 12 --score 50 --sort rate -p https --country "$INPUT_REFLECTOR_COUNTRY"  --save /etc/pacman.d/mirrorlist
