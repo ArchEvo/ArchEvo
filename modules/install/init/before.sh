0ffcae86618f66cffdfcfa17a0590f4f6105c590
StellarCollision <stellarcollision@logbuch.it>
2021-01-17 16:37:04 +0100

    haveged: install and start haveged before installing the system.

--- modules/install/init/before.sh
+++ modules/install/init/before.sh
@@ -2,3 +2,7 @@
 
 timedatectl set-timezone $INPUT_TIMEZONE_CONTINENT/$INPUT_TIMEZONE_CITY
 timedatectl set-ntp true
+
+pacman -Sy --noconfirm haveged
+systemctl daemon-reload
+systemctl start haveged
