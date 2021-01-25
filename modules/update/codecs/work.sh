0047a02cd55dee3a4bdba76cd92e556c86c8afe9
StellarCollision <stellarcollision@logbuch.it>
2021-01-26 00:21:04 +0100

    codecs: add more video codecs

--- modules/update/codecs/work.sh
+++ modules/update/codecs/work.sh
@@ -8,7 +8,7 @@ pacman -S --noconfirm --needed jasper libwebp
 pacman -S --noconfirm --needed flac wavpack lame libmad opus libvorbis libdca faad2 libfdk-aac
 
 #video codecs
-pacman -S --noconfirm --needed ffmpeg gstreamer gst-plugins-good gst-libav libavif libheif aom dav1d rav1e libde265 libdv libmpeg2 libtheora schroedinger libvpx x264 x265 xvidcore
+pacman -S --noconfirm --needed ffmpeg gstreamer gst-plugins-base gst-plugins-ugly gst-plugins-good gst-libav libavif libheif aom dav1d rav1e libde265 libdv libmpeg2 libtheora schroedinger libvpx x264 x265 xvidcore
 
 
 #Hardware video acceleration:
@@ -29,5 +29,5 @@ fi
 
 #NVIDIA (closesource)
 if [ "$INPUT_GPU_DRIVER" == "nvidia" ] || [ "$INPUT_GPU_DRIVER" == "nvidia-lts" ]; then
-    pacman -S --noconfirm --needed nvidia-utils
+    pacman -S --noconfirm --needed nvidia-utils gst-plugins-bad 
 fi
