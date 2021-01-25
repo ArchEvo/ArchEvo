a7e8fd89394073abdb89dcf08d7c4be06d2d261a
StellarCollision <stellarcollision@logbuch.it>
2021-01-26 00:21:48 +0100

    font: add more fonts

--- modules/update/font/work.sh
+++ modules/update/font/work.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-pacman -S --noconfirm --needed freetype2 ttf-font-awesome ttf-opensans gnu-free-fonts ttf-dejavu ttf-caladea font-bh-ttf ttf-roboto noto-fonts-extra noto-fonts ttf-croscore ttf-liberation noto-fonts-cjk ttf-ubuntu-font-family ttf-bitstream-vera noto-fonts-emoji 
+pacman -S --noconfirm --needed xorg-mkfontscale 
+pacman -S --noconfirm --needed freetype2 ttf-font-awesome ttf-opensans gnu-free-fonts ttf-dejavu ttf-caladea font-bh-ttf ttf-roboto noto-fonts-extra noto-fonts ttf-croscore ttf-liberation noto-fonts-cjk ttf-ubuntu-font-family ttf-bitstream-vera noto-fonts-emoji ttf-hannom ttf-khmer otf-ipafont ttf-hanazono ttf-baekmuk ttf-tibetan-machine 
