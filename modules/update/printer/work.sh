62d66f03917d09dad26dcd326517bc0234cc8b00
StellarCollision <stellarcollision@logbuch.it>
2021-01-28 00:13:07 +0100

    printer: add python packages that hp needs

--- modules/update/printer/work.sh
+++ modules/update/printer/work.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pacman -S --noconfirm --needed cups cups-pdf ghostscript gsfonts hplip system-config-printer foomatic-db foomatic-db-nonfree foomatic-db-nonfree-ppds foomatic-db-ppds foomatic-db-engine gutenprint foomatic-db-gutenprint-ppds gsfonts bluez-cups xsane simple-scan
+pacman -S --noconfirm --needed cups cups-pdf ghostscript gsfonts hplip system-config-printer foomatic-db foomatic-db-nonfree foomatic-db-nonfree-ppds foomatic-db-ppds foomatic-db-engine gutenprint foomatic-db-gutenprint-ppds gsfonts bluez-cups xsane simple-scan python-notify2 python-pillow python-reportlab python-pyqt5
 
 systemctl enable cups.service
