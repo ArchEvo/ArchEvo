569af5eca79010eacc2b71132147c0453c774fa9
StellarCollision <stellarcollision@logbuch.it>
2021-01-27 23:45:32 +0100

    gnome: hide software that is not needed

--- modules/update/gnome/after_check.sh
+++ modules/update/gnome/after_check.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+if ! grep -q NoDisplay=true /usr/share/applications/qv4l2.desktop; then 
+  echo NoDisplay=true >> /usr/share/applications/qv4l2.desktop
+fi
+
+if ! grep -q NoDisplay=true /usr/share/applications/qvidcap.desktop; then 
+  echo NoDisplay=true >> /usr/share/applications/qvidcap.desktop 
+fi
+
+if ! grep -q NoDisplay=true /usr/share/applications/bssh.desktop; then 
+  echo NoDisplay=true >> /usr/share/applications/bssh.desktop
+fi
+
+if ! grep -q NoDisplay=true /usr/share/applications/bvnc.desktop; then 
+  echo NoDisplay=true >> /usr/share/applications/bvnc.desktop
+fi
+
+if ! grep -q NoDisplay=true /usr/share/applications/avahi-discover.desktop; then 
+  echo NoDisplay=true >> /usr/share/applications/avahi-discover.desktop
+fi
