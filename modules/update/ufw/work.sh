c3967ee2715867ce339e24021667b262804fcf82
StellarCollision <stellarcollision@logbuch.it>
2021-01-27 22:24:29 +0100

    ufw: enable helpers

--- modules/update/ufw/work.sh
+++ modules/update/ufw/work.sh
@@ -6,5 +6,8 @@ systemctl enable ufw
 sed -i 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
 ufw enable
 
+# enable helpers
+echo "options nf_conntrack nf_conntrack_helper=1" > /etc/modprobe.d/nf_conntrack.conf
+
 # regulate
 ufw allow CIFS
