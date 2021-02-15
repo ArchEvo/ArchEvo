22628de088550e2f06d0a0eeca342dc5e8cd97c4
StellarCollision <stellarcollision@logbuch.it>
2021-02-16 00:56:24 +0100

    gnome: categorized

--- modules/update/gnome/work.sh
+++ modules/update/gnome/work.sh
@@ -6,13 +6,25 @@ pacman -S --noconfirm --needed gnome gnome-tweaks gdm archlinux-wallpaper libope
 systemctl enable gdm
 
 #-->Config
-su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.input-sources sources \"[('xkb', 'de')]\""
-su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.software download-updates false"
+#keyboard
 su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.peripherals.keyboard remember-numlock-state true"
 su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true"
+
+
+#mouse
+su $INPUT_USER_NAME -c "gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true"
+
+
+#user interface
+su $INPUT_USER_NAME -c "gsettings set org.gnome.desktop.interface show-battery-percentage true"
 su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.screensaver picture-uri 'file:///usr/share/backgrounds/archlinux/simple.png'"
 su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.screensaver picture-opacity 100"
 su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.screensaver picture-options 'zoom'"
 su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/archlinux/simple.png'"
 su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.background picture-options 'zoom'"
 su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.background picture-opacity 100"
+
+
+#functions
+su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.desktop.input-sources sources \"[('xkb', 'de')]\""
+su $INPUT_USER_NAME -c "dbus-launch --exit-with-session gsettings set org.gnome.software download-updates false"
