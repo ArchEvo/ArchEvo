f057fb64ad864de9d0503a093cf25988c1344071
StellarCollision <stellarcollision@logbuch.it>
2021-02-15 19:00:06 +0100

    net_tools: add wget

--- modules/update/net_tools/work.sh
+++ modules/update/net_tools/work.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S --noconfirm --needed dnsutils net-tools iptraf-ng net-tools inetutils gnu-netcat nmap
+pacman -S --noconfirm --needed dnsutils net-tools iptraf-ng net-tools inetutils gnu-netcat nmap wget
