e28cf2375b049ee1717a8692f2ee55706711e59d
StellarCollision <stellarcollision@logbuch.it>
2021-03-02 21:12:14 +0100

    utilities: add whois

--- modules/update/utilities/work.sh
+++ modules/update/utilities/work.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S --noconfirm --needed inetutils bind-tools unrar unzip p7zip iotop sysstat man man-pages bash-completion lsof lostfiles
+pacman -S --noconfirm --needed inetutils bind-tools unrar unzip p7zip iotop sysstat man man-pages bash-completion lsof lostfiles whois
