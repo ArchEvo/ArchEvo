66f9f171adf51166b3b43142185747c8ad786426
StellarCollision <stellarcollision@logbuch.it>
2021-03-02 21:11:16 +0100

    utilities: add lostfiles

--- modules/update/utilities/work.sh
+++ modules/update/utilities/work.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S --noconfirm --needed inetutils bind-tools unrar unzip p7zip iotop sysstat man man-pages bash-completion lsof
+pacman -S --noconfirm --needed inetutils bind-tools unrar unzip p7zip iotop sysstat man man-pages bash-completion lsof lostfiles
