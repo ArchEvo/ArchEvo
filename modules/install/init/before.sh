ea5cab33da3fa13a5868e87fad36348e71288fc1
StellarCollision <stellarcollision@logbuch.it>
2021-03-25 23:30:52 +0100

    restart systemd-timesyncd before installation

--- modules/install/init/before.sh
+++ modules/install/init/before.sh
@@ -3,7 +3,8 @@
 #load kernel module
 modprobe ip6table_filter #required by ufw for new rules
 
-
+systemctl restart systemd-timesyncd
+sleep 1
 timedatectl set-timezone $INPUT_TIMEZONE_CONTINENT/$INPUT_TIMEZONE_CITY
 timedatectl set-ntp true
 
