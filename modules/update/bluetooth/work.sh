58b188722df3be796c6c56282f0b702df20643c1
StellarCollision <stellarcollision@logbuch.it>
2021-07-01 17:40:06 +0200

    bluez: add bluez-hid2hci

--- modules/update/bluetooth/work.sh
+++ modules/update/bluetooth/work.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pacman -S --noconfirm --needed bluez bluez-utils bluez-libs bluez-plugins 
+pacman -S --noconfirm --needed bluez bluez-utils bluez-libs bluez-plugins bluez-hid2hci
 systemctl enable bluetooth.service
