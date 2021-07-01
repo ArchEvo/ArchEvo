f2d12c54b536af27ba933ed6c8813b7632c609a5
StellarCollision <stellarcollision@logbuch.it>
2021-07-01 17:46:17 +0200

    utilities: add many utilities

--- modules/update/utilities/work.sh
+++ modules/update/utilities/work.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-pacman -S --noconfirm --needed inetutils bind-tools unrar unzip p7zip iotop sysstat man man-pages bash-completion lsof lostfiles whois ncdu htop lhasa
+# filesystem
+pacman -S --noconfirm --needed iotop sysstat lsof lostfiles ncdu
+pacman -S --noconfirm --needed cifs-utils exfat-utils nfs-utils nilfs-utils
+
+# compress
+pacman -S --noconfirm --needed unrar unzip p7zip lhasa
+
+# network
+pacman -S --noconfirm --needed inetutils iputils bind-tools iotop sysstat whois tcpdump
+
+# prozess
+pacman -S --noconfirm --needed sysstat htop top
+
+# scripting
+pacman -S --noconfirm --needed jq
+
+# hardware
+pacman -S --noconfirm --needed smartmontools
