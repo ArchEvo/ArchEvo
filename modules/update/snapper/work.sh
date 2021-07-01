0361a675b0f6b7cfbf246d6666eb31e37fe1f0a8
StellarCollision <stellarcollision@logbuch.it>
2021-07-01 18:06:00 +0200

    snapper: typo fix

--- modules/update/snapper/work.sh
+++ modules/update/snapper/work.sh
@@ -2,6 +2,7 @@
 
 pacman -S --noconfirm --needed snapper snap-pac grub-btrfs cronie
 
+# snap-pac-grub muss noch configuriert werden und verstanden ...
 
 # set config
 sed -i 's/TIMELINE_LIMIT_HOURLY="10"/TIMELINE_LIMIT_HOURLY="0"/' /etc/snapper/configs/root
@@ -11,7 +12,7 @@ sed -i 's/TIMELINE_LIMIT_YEARLY="10"/TIMELINE_LIMIT_YEARLY="1"/' /etc/snapper/co
 sed -i 's/TIMELINE_CREATE="yes"/TIMELINE_CREATE="no"/' /etc/snapper/configs/root
 
 # enable snapper services
-systemctl enable cron.service
+systemctl enable cronie.service
 systemctl enable snapper-cleanup.timer
 systemctl enable snapper-timeline.timer
 systemctl enable snapper-boot.timer
