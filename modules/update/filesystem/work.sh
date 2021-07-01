5d99e97d282d38971758a947a156eb14e1307bc3
StellarCollision <stellarcollision@logbuch.it>
2021-07-01 23:57:40 +0200

    filesystem: conflict resolved

--- modules/update/filesystem/work.sh
+++ modules/update/filesystem/work.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-pacman -S --noconfirm --needed btrfs-progs dosfstools exfatprogs exfat-utils f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs squashfs-tools nfs-utils mtpfs
+pacman -S --noconfirm --needed btrfs-progs dosfstools exfatprogs f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs squashfs-tools nfs-utils mtpfs
 
 # enable essential services
 systemctl enable btrfs-scrub@-.timer
 systemctl enable btrfs-scrub@home.timer
+#systemctl enable btrfs-scrub@swap.timer # Good idea?
 systemctl enable btrfs-scrub@snapshots.timer
