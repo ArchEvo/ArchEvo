f568d71aa44dd59c954bc318b3a892fbb6589ca5
StellarCollision <stellarcollision@logbuch.it>
2021-07-01 17:52:29 +0200

    filesystem: add btrfs scrub

--- modules/update/filesystem/work.sh
+++ modules/update/filesystem/work.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 pacman -S --noconfirm --needed btrfs-progs dosfstools exfatprogs exfat-utils f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs squashfs-tools nfs-utils mtpfs
+
+# enable essential services
+systemctl enable btrfs-scrub@-.timer
+systemctl enable btrfs-scrub@home.timer
+systemctl enable btrfs-scrub@snapshots.timer
